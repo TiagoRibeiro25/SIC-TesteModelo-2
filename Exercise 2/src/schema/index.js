81a1c804b2120741832078a17c874ef02be483ae
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-12-29 23:42:42 +0000

    Replaced spaces with tabs

--- Exercise 2/src/schema/index.js
+++ Exercise 2/src/schema/index.js
@@ -1,26 +1,26 @@
 const Apollo = require("apollo-server-express");
 
 module.exports = Apollo.gql`
-    type Query {
+	type Query {
 		getStation(id: ID!): WeatherStation!
-    }
+	}
 
-    type RainfallRecord {
-        id: ID!,
-        value: Float!,
+	type RainfallRecord {
+		id: ID!,
+		value: Float!,
 		dateTime: String!,
-    }
+	}
 
-    type WeatherStation {
-        id: ID!,
-        name: String!,
+	type WeatherStation {
+		id: ID!,
+		name: String!,
 		description: String,
 		latitude: Float!,
 		longitude: Float!,
 		records: [RainfallRecord!],
-    }
+	}
 
-    type Mutation {
+	type Mutation {
 		addStation(name: String!, description: String, latitude: Float!, longitude: Float!): WeatherStation!
 	}
 `;
